fb2fecc31af24a1cd891376ba934f45a555c0e40
Gao-Ziyu <gaoziyu528@gmail.com>
2023-12-31 02:58:17 +0800

    added enter button

---
+++ Assets/Script/SceneData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneData : MonoBehaviour
+{
+    public static int LPS_Ext = 0;
+    public static int LPS_Int = 1;
+}
