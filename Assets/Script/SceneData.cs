a2f3cbfbf11e090a4e8158791f2b02ac06f68be9
Gao-Ziyu <gaoziyu528@gmail.com>
2024-01-04 15:09:26 +0800

    fix bugs

--- Assets/Script/SceneData.cs
+++ Assets/Script/SceneData.cs
@@ -6,8 +6,8 @@ public class SceneData : MonoBehaviour
 {
     public static int LPS_Ext = 0;
     public static int LPS_Int = 1;
-    public static int present = 2;
-    public static int past = 3;
+    public static int present = 0;
+    public static int past = 1;
     public static int End = 1;
     public static int Auth = 0;
 }
