92809323f452a23c5bd5b1751fb0ee32de5247e3
Gao-Ziyu <gaoziyu528@gmail.com>
2024-01-05 17:23:12 +0800

    update kopi interactions
    - kopi interactions
    - tried fix ui

--- Assets/Script/AnimationKopi.cs
+++ Assets/Script/AnimationKopi.cs
@@ -7,18 +7,46 @@ public class AnimationKopi : MonoBehaviour
     public Animator conMilk;
     public Transform teaSet;
 
+    public Animator pourKopi;
+
+    public GameObject conMilky;
+    public GameObject doneKopi;
     public GameObject condenseMilk;
+    public GameObject kopi;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "condenseMilk")
         {
+            conMilky.SetActive(true);
             conMilk.SetBool("Pour", true);
             other.transform.SetParent(teaSet);
             condenseMilk.SetActive(true);
-            Destroy(other.gameObject);
+            StartCoroutine(hideCondenseMilk());
+        }
+
+        if(other.gameObject.tag == "doneKopi")
+        {
+            doneKopi.SetActive(true);
+            pourKopi.SetBool("donePour", true);
+            other.transform.SetParent(teaSet);
+            kopi.SetActive(true);
+            StartCoroutine(hideKopi());
         }
     }
+
+    IEnumerator hideCondenseMilk()
+    {
+        yield return new WaitForSeconds(2);
+        conMilky.SetActive(false);
+    }
+
+    IEnumerator hideKopi()
+    {
+        yield return new WaitForSeconds(2);
+        doneKopi.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Script/MakingKopi.cs
+++ Assets/Script/MakingKopi.cs
@@ -7,16 +7,56 @@ public class MakingKopi : MonoBehaviour
     public Animator kopiPowderPour;
     public Transform MakeKopi;
 
+    public Animator hotWaterPour;
+
+    public Animator stir;
+
     public GameObject kopiPowder;
     public GameObject hotWater;
+    public GameObject scooper;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "condenseMilk")
+        if (other.gameObject.tag == "kopiPowder")
         {
-            kopiPowderPour.SetBool("Pour", true);
+            kopiPowder.SetActive(true);
+            kopiPowderPour.SetBool("kopiPour", true);
             other.transform.SetParent(MakeKopi);
-            Destroy(other.gameObject);
+            StartCoroutine(hideKopiPowder());
         }
+
+        if (other.gameObject.tag == "hotWater")
+        {
+            hotWater.SetActive(true);
+            hotWaterPour.SetBool("hotpour", true);
+            other.transform.SetParent(MakeKopi);
+            StartCoroutine(hideHotWater());
+        }
+
+        if (other.gameObject.tag == "scooper")
+        {
+            scooper.SetActive(true);
+            stir.SetBool("stir", true);
+            other.transform.SetParent(MakeKopi);
+            StartCoroutine(hideScooper());
+        }
+    }
+
+    IEnumerator hideKopiPowder()
+    {
+        yield return new WaitForSeconds(2);
+        kopiPowder.SetActive(false);
+    }
+
+    IEnumerator hideHotWater()
+    {
+        yield return new WaitForSeconds(2);
+        hotWater.SetActive(false);
+    }
+
+    IEnumerator hideScooper()
+    {
+        yield return new WaitForSeconds(2);
+        scooper.SetActive(false);
     }
 }
