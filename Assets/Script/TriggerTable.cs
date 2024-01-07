51619d44c4b8a9981028abb3af0de607c9e08be9
LeongYenZhen <S10221750@connect.np.edu.sg>
2024-01-07 19:30:40 +0800

    test

--- Assets/Script/TriggerTable.cs
+++ Assets/Script/TriggerTable.cs
@@ -32,16 +32,4 @@ public class TriggerTable : MonoBehaviour
             animMilk.SetActive(true);
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
